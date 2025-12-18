5e17bc35705c64fccd9e0d4e3f34b9b4da2dd650
bala-amypo <bala@amypo.in>
2025-12-18 08:16:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/Category.java
+++ src/main/java/com/example/demo/entity/Category.java
@@ -1,14 +1,19 @@
 
 
 package com.example.demo.entity;
+import java.util.*;
+
 
 
 public class CategoryEntity{
     private Long id;
     private String name;
     private String unit;
-    private  String costPerUnit;
+    private String costPerUnit;
     private boolean active;
-    private LocatDateTime createdAt;
-    private LocatDateTime updatedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    
+
 }
