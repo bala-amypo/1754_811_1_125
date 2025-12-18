34b2c5850a16200c9237ac1c72202e0364268501
bala-amypo <bala@amypo.in>
2025-12-18 09:41:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/Category.java
+++ src/main/java/com/example/demo/entity/Category.java
@@ -11,7 +11,7 @@ public class CategoryEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    
     private String name;
 
     private String unit;
