e7ba5ef89521247f20e64bb573b6c42b02bf50f6
bala-amypo <bala@amypo.in>
2025-12-18 09:37:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Category.java
+++ src/main/java/com/example/demo/entity/Category.java
@@ -1,55 +1,88 @@
-
-
 package com.example.demo.entity;
-import java.util.*;
+
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-public class CategoryEntity{
+@Table(name = "categories")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
     private String unit;
+
     private String costPerUnit;
+
     private boolean active;
+
+   
     private LocalDateTime createdAt;
+
+    
     private LocalDateTime updatedAt;
 
+    // ===== Getters and Setters =====
 
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getName(String name){
-        return this.name;
+    public Long getId() {
+        return id;
     }
 
-
-    public void setUnit(String unit){
-        this.unit=unit;
-    }
-    public String getUnit(String unit){
-        return  this.unit;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public void setCostPerUnit(String costPerUnit){
-        this.costPerUnit=costPerUnit;
+    public String getName() {
+        return name;
     }
-    public String GetCostPerUnit(String costPerUnit){
-         return this.costPerUnit;
-     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public void setActive(boolean active){
-        this.active=active;
+    public String getUnit() {
+        return unit;
     }
-    public void setCreatedAt( LocalDateTime createdAt){
-        this.createdAt=createdAt;
+
+    public void setUnit(String unit) {
+        this.unit = unit;
     }
+
+    public String getCostPerUnit() {
+        return costPerUnit;
+    }
+
+    public void setCostPerUnit(String costPerUnit) {
+        this.costPerUnit = costPerUnit;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
     
-    public void setUpdatedAt( LocalDateTime updatedAt){
-        this.updatedAt=updatedAt;
-    }
-    
-
-
-
 }
