3d5daa6e721e42abbfae1f64f6d07002a0805c63
bala-amypo <bala@amypo.in>
2025-12-18 16:12:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RecipeIngredientServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RecipeIngredientServiceImpl.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.entity.RecipeIngredientEntity;
 import com.example.demo.repository.RecipeIngredientRepository;
 import com.example.demo.service.RecipeIngredientService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RecipeIngredientServiceImpl implements RecipeIngredientService {
     @Autowired
     RecipeIngredientRepository repo;
