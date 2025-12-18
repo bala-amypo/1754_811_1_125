1c953421624fcf0ce0abd303cb9a256ca910926f
bala-amypo <bala@amypo.in>
2025-12-18 13:19:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,11 @@
 package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import com.example.demo.entity.CategoryEntity;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.service.CategoryService;
-
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
