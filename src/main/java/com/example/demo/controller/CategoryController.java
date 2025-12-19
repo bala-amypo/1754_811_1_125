aa64500763496156e3e2b9b28b4334a162bbe8b2
bala-amypo <bala@amypo.in>
2025-12-19 12:18:00 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.CategoryEntity;
 import com.example.demo.service.CategoryService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -15,8 +21,30 @@ public class CategoryController {
     CategoryService ser;
     
     
-    @PostMapping("/addcategory")
-    public CategoryEntity addcategory(@RequestBody CategoryEntity std){
-        return ser.addcategory(std);
+    @PostMapping("/createCategory")
+    public CategoryEntity createCategory(@RequestBody CategoryEntity category){
+        return ser.createCategory(category);
+
     }
+    @PutMapping("/updateCategory/{id}")
+    public CategoryEntity updateCategory(@PathVariable Long id,@RequestBody CategoryEntity category){
+        return ser.updateCategory(id,category);
+    }
+    @GetMapping("/getCategoryById/{id}")
+    public CategoryEntity getCategoryById(@PathVariable Long id){
+        return  ser.getCategoryById(id);
+
+    }
+    @GetMapping("/getAllCategories")
+    public List<CategoryEntity> getAllCategories(){
+        return ser.getAllCategories();
+    }
+    @GetMapping("/deActiveCategory/{id}")
+    public CategoryEntity deActiveCategory(@PathVariable Long id){
+        return  ser.deActiveCategory(id);
+
+    }
+    
+
+    
 }
--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,17 +1,44 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.CategoryEntity;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.service.CategoryService;
-@Service
+
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     CategoryRepository repo;
-    public CategoryEntity addcategory (CategoryEntity std){
-        return repo.save(std);
+    public CategoryEntity createCategory( CategoryEntity category){
+        return repo.save(category);
     }
+    public CategoryEntity updateCategory( Long id, CategoryEntity category){
+        CategoryEntity newval=repo.findById(id).orElseThrow(()->new ResourceNotFoundException("no new id is found"));
+        newval.setName(category.getName());
+        newval.setDescription(category.getDescription());
+        newval.setActive(category.isActive());
+        return repo.save(newval);
+
+    }
+    public CategoryEntity getCategoryById( Long id){
+        return repo.findById(id).orElseThrow(()-> new ResourceNotFoundException( "no new id is found"));
+    
+    }
+    public List<CategoryEntity> getAllCategories(){
+        return repo.findAll();
+    }
+     public CategoryEntity deActiveCategory(Long id){
+        CategoryEntity category=repo.findById(id).orElseThrow(()->new ResourceNotFoundException("no id is found"));
+        if(category.isActive()==true){
+            category.setActive(false);
+        }
+        return repo.save(category);
+
+     }
+
     
 }
