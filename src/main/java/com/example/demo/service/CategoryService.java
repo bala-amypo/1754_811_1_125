dd8664b0ac03c22c54094a1d06e835c1ac970336
bala-amypo <bala@amypo.in>
2025-12-19 12:21:00 +0000

    User commit

--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -1,9 +1,19 @@
 package com.example.demo.service;
 
 
+
+
+import java.util.List;
+
+
+
 import com.example.demo.entity.CategoryEntity;
 
 public interface CategoryService {
-    public CategoryEntity addcategory(CategoryEntity std);
+     public CategoryEntity createCategory( CategoryEntity category);
+     public CategoryEntity updateCategory( Long id, CategoryEntity category);
+     public CategoryEntity getCategoryById( Long id);
+     public List<CategoryEntity> getAllCategories();
+     public CategoryEntity deActiveCategory(Long id);
     
 }
