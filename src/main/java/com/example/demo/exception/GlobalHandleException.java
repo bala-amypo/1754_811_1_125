affddb73ba41b9873d83d9bfd2e7b086c6ab4e94
bala-amypo <bala@amypo.in>
2025-12-26 16:21:32 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalHandleException.java
+++ src/main/java/com/example/demo/exception/GlobalHandleException.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalRequestException{
+public class GlobalHandleException{
 
     
     @ExceptionHandler(ResourceNotFoundException.class)
